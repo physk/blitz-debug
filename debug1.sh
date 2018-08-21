fcb20011fe52211c56b0154a658039bf323d9b62
PhysK <chris@physk.co.uk>
2018-08-21 02:55:33 +0100

    Extra Stuff :)

--- debug1.sh
+++ debug1.sh
@@ -10,8 +10,12 @@ echo "Found `ls -la /opt/appdata/pgblitz/keys/unprocessed/ | awk '{print $9}' | 
 echo "---- Bad Keys (Manual) ----"
 ls -la /opt/appdata/pgblitz/keys/badjson/ | awk '{print $9}' | egrep '(PG|GD|GS)'
 echo "Found `ls -la /opt/appdata/pgblitz/keys/badjson/ | awk '{print $9}' | egrep '(PG|GD|GS)' | wc -l` Keys under badjson"
+echo "---- Folders ----"
+echo "Found `find /mnt/pgblitz -type d | wc -l` files Under /mnt/pgblitz"
 echo "---- Files ----"
 echo "Found `find /mnt/move -type f | wc -l` files Under /mnt/move"
 echo "Found `find /mnt/pgblitz -type f | wc -l` files Under /mnt/pgblitz"
+echo "Found `find /opt/appdata/pgblitz/pid -type f -iname '*.trans' | wc -l` PID files"
+echo "Found `find /mnt/move -type f -iname '*.lck' | wc -l ` lock files in /mnt/move"
 echo "---- Journalctl ----"
 cat /opt/appdata/plexguide/pg.log | grep PGBlitz | tail -100
